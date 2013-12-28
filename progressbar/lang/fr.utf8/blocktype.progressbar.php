d05cbca6ab5ac7f7b548c028dd2978d2aa3a3c77
Dominique-Alain JAN <djanmac.com>
2013-12-28 18:13:11 +0100

    Added the French translation

---
+++ progressbar/lang/fr.utf8/blocktype.progressbar.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2013 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-progressbar
+ * @author     Gregor Anzelj
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2013 Gregor Anzelj <gregor.anzelj@gmail.com>
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Compléter le profil';
+
+$string['completiontips'] = 'Conseils pour compléter votre profil';
+$string['addprofilepicture'] = 'Ajoutez votre portrait';
+$string['addpersonalinformation'] = 'Complétez les infomrations personnelles de base';
+$string['addcontactinformation'] = 'Ajoutez une adresse pour pouvoir vous joindre';
+$string['addmessaging'] = 'Ajouter une adresse de messagerie électronique';
+$string['addselfdescription'] = 'Ajoutez un texte de présentation';
+$string['addemployment'] = 'Complétez l\'historique de vos formation et emplois';
+$string['addachievements'] = 'Ajoutez une liste de buts déjà atteints';
+$string['addgoals'] = 'Indiquez les buts que vous désirez encore atteindre';
+$string['addskills'] = 'Faites la liste de vos compétences';
+$string['addinterests'] = 'Faites la liste de vos intérêts';
